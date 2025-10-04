5147c136ac91813571275b20fb1a68f52e1f9f5b
Andrey Lomovtsev <andrei150202@gmail.com>
2025-10-04 23:13:35 +0300

    iter1: create service struct and implementation

---
+++ internal/service/service.go
@@ -0,0 +1,34 @@
+// Package service implemets core logic of URL shortener
+package service
+
+import "errors"
+
+type Service struct {
+	urls map[string]string
+}
+
+func NewService() Service {
+	return Service{
+		urls: make(map[string]string),
+	}
+}
+
+func (s Service) AddURL(url string) string {
+	shortURL := generateURL()
+	for _, ok := s.urls[shortURL]; ok; {
+		shortURL = generateURL()
+	}
+
+	s.urls[shortURL] = url
+
+	return shortURL
+}
+
+func (s Service) GetURL(shortURL string) (string, error) {
+	url, ok := s.urls[shortURL]
+
+	if !ok {
+		return "", errors.New("URL not found")
+	}
+	return url, nil
+}
