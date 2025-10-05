e0a5c1e1d0290a2cbba0ec6ff0cb68337805191c
Andrey Lomovtsev <andrei150202@gmail.com>
2025-10-05 18:04:42 +0300

    iter1: write main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/darrior/urlshortener/internal/handler"
+	"github.com/darrior/urlshortener/internal/service"
+)
+
+func main() {
+	s := service.NewService()
+	srv := handler.NewServer("127.0.0.1:8080", s)
+	if err := srv.Run(); err != nil {
+		fmt.Printf("An error occured: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
