2b69319238d75db87ffbd50654b3564e834c7f0c
Andrey Lomovtsev <andrei150202@gmail.com>
2025-11-17 22:22:22 +0300

    iter12: add new method to Repository interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,10 +4,14 @@ package repository
 import (
 	"context"
 	"errors"
+
+	"github.com/darrior/urlshortener/internal/models"
 )
 
 type Repository interface {
 	AddURL(ctx context.Context, id, url string) (err error)
+	AddURLs(ctx context.Context, batchURLs models.BatchURLs) (err error)
+	Count(ctx context.Context) (count int, err error)
 	GetURL(ctx context.Context, id string) (url string, err error)
 	Ping(ctx context.Context) (err error)
 	Close() (err error)
