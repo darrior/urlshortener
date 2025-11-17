c87fba7bec4766e8f3b0708782d5cbc08a554e39
Andrey Lomovtsev <andrei150202@gmail.com>
2025-11-17 23:46:28 +0300

    iter12: add tests for maprepository new methods

--- internal/repository/maprepository_test.go
+++ internal/repository/maprepository_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/darrior/urlshortener/internal/models"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -56,6 +57,49 @@ func TestMapRepository_AddURL(t *testing.T) {
 	}
 }
 
+func TestMapRepository_AddURLs(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		batchURLs models.BatchURLs
+		want      map[string]string
+		wantErr   bool
+	}{
+		{
+			name: "Test AddURLs",
+			batchURLs: models.BatchURLs{
+				{
+					ID:  "abc",
+					URL: "123",
+				},
+				{
+					ID:  "cba",
+					URL: "321",
+				},
+			},
+			want: map[string]string{
+				"abc": "123",
+				"cba": "321",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := NewMapRepository()
+			gotErr := m.AddURLs(context.Background(), tt.batchURLs)
+
+			if tt.wantErr {
+				assert.Error(t, gotErr)
+				return
+			}
+
+			assert.NoError(t, gotErr)
+			assert.Equal(t, tt.want, m.urls)
+		})
+	}
+}
+
 func TestMapRepository_GetURL(t *testing.T) {
 	tests := []struct {
 		name string // description of this test case
@@ -92,6 +136,80 @@ func TestMapRepository_GetURL(t *testing.T) {
 
 			assert.NoError(t, gotErr)
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestMapRepository_Count(t *testing.T) {
+	tests := []struct {
+		name    string // description of this test case
+		urls    map[string]string
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "Empty map",
+			urls:    map[string]string{},
+			want:    0,
+			wantErr: false,
+		},
+		{
+			name: "Non empty map",
+			urls: map[string]string{
+				"abc": "123",
+				"cba": "321",
+			},
+			want:    2,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := MapRepository{
+				urls: tt.urls,
+			}
+			got, gotErr := m.Count(context.Background())
+
+			if tt.wantErr {
+				assert.Error(t, gotErr)
+				return
+			}
+
+			assert.NoError(t, gotErr)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestMapRepository_Ping(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+	}{
+		{
+			"Always valid",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := NewMapRepository()
+			gotErr := m.Ping(context.Background())
+			assert.NoError(t, gotErr)
+		})
+	}
+}
+
+func TestMapRepository_Close(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+	}{
+		{"Always valid"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := NewMapRepository()
+			gotErr := m.Close()
+
+			assert.NoError(t, gotErr)
 		})
 	}
 }
