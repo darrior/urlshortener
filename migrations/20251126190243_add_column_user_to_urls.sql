fa288604f184e0c014d154a8757310ff4a2c865d
Andrey Lomovtsev <andrei150202@gmail.com>
2025-11-30 19:51:43 +0300

    iter14: add new migrations

---
+++ migrations/20251126190243_add_column_user_to_urls.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE urls
+ADD COLUMN user_id;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE urls
+DROP COLUMN user_id;
+-- +goose StatementEnd
