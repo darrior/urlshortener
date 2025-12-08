c8d4363c6baa3004c605dbfccd7d0fb37aaccd2b
Andrey Lomovtsev <andrei150202@gmail.com>
2025-12-08 19:55:41 +0300

    iter14: use auth middlware on all routes

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -4,6 +4,7 @@ func (s *Server) addRoutes() {
 	s.mux.Use(logMiddlware)
 	s.mux.Use(compressMiddlware)
 	s.mux.Use(extractMiddlware)
+	s.mux.Use(s.h.authCookieMiddlware)
 
 	s.mux.HandleFunc("/", s.h.errorHandler)
 
@@ -11,13 +12,7 @@ func (s *Server) addRoutes() {
 	s.mux.Get("/ping", s.h.getPing)
 	s.mux.Get("/api/user/urls", s.h.getAPIUserURLs)
 
-	s.mux.
-		With(s.h.authCookieMiddlware).
-		Post("/", s.h.postURL)
-	s.mux.
-		With(s.h.authCookieMiddlware).
-		Post("/api/shorten", s.h.postAPIShorten)
-	s.mux.
-		With(s.h.authCookieMiddlware).
-		Post("/api/shorten/batch", s.h.postAPIShortenBatch)
+	s.mux.Post("/", s.h.postURL)
+	s.mux.Post("/api/shorten", s.h.postAPIShorten)
+	s.mux.Post("/api/shorten/batch", s.h.postAPIShortenBatch)
 }
